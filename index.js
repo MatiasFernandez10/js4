a76be834238c2b7bc9b13a2602b2844b50a2e66b
MatiasFernandez10 <matiasfernandezari@gmail.com>
2025-03-17 17:49:34 -0300

    mi cuarto commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function boton(){
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
+        'E', 'T'];
+    var dni = 49255638;
+    var letra = letras[dni/23];
+    console.log(letra);
+}
