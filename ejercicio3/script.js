7105c89d43161b57c1bc570827f254096a48308d
MatiasFernandez10 <matiasfernandezari@gmail.com>
2025-04-14 17:31:21 -0300

    Update script.js

--- ejercicio3/script.js
+++ ejercicio3/script.js
@@ -9,4 +9,5 @@ function boton(){
         console.log("numero1 es negativo o distinto de cero")
     if((numero1+1) < numero2)
         console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2")
+        
 }
