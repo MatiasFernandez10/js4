e2e1b0e9db1fe7fde2a0f6b1518d1bde715021dc
MatiasFernandez10 <matiasfernandezari@gmail.com>
2025-04-14 17:12:00 -0300

    Update script.js

--- ejercicio6/script.js
+++ ejercicio6/script.js
@@ -1,12 +1,8 @@
 function boton(){
-    var num = 6, n = 0
+    var num = parseInt(prompt("Introduzca un numero"));
 
-    while(n<num){
-        n = n+2
-        console.log(n)
-        if(num == n)
-            console.log("El numero es par")
-    }
-    if(n > num)
-        console.log("El numero es impar")
+    if(num % 2 == 0)
+        console.log("Par")
+    else
+    console.log("Inpar")
 }
