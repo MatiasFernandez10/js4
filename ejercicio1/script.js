8c9cd829d1daacf77c483ab1fe1357ca97f2cbc7
unknown <sergiomvp08@gmail.com>
2025-04-14 15:14:29 -0300

    14-4

---
+++ ejercicio1/script.js
@@ -0,0 +1,7 @@
+function botone(){
+    let arr = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+
+    for(let i=0; i<=12; i++){
+        console.log(arr[i])
+    }
+}
